78bb5867a8bcbdebc50eca71cb7ad9533b4f6e70
mgostnell <mgostnell@XC-BZN-MBG.LOCAL.LAN>
2016-09-13 18:57:07 -0600

    Generator now creates n unique points within a unit square Need to finalize generateEdges() before Generator is complete Added generateNewGraph(int numPnts) for reuse of the Generator class

--- src/csci/pkg446/project1/Driver.java
+++ src/csci/pkg446/project1/Driver.java
@@ -6,22 +6,8 @@ package csci.pkg446.project1;
  * @author Mathew Gostnell
  */
 public class Driver {
-    public static void main(String[] args) {
-        //TODO here
-        Point p1 = new Point(0.0, 0.0);
-        Point p2 = new Point(1.0, 1.0);
-        Edge e1 = new Edge(p1, p2);
-        e1.print();
-        System.out.println("y = " + e1.slope() + "x + " + e1.intercept());
-        
-        Point p3 = new Point(1.0, 0.0);
-        Point p4 = new Point(0.0, 1.0);
-        Edge e2 =  new Edge(p3, p4);
-        e2.print();
-        System.out.println("y = " + e2.slope() + "x + " + e2.intercept());
-        
+    public static void main(String[] args) {        
         Generator g = new Generator(50);
-        System.out.println(g.isIntercept(e1, e2));
         
     }
 }
--- src/csci/pkg446/project1/Generator.java
+++ src/csci/pkg446/project1/Generator.java
@@ -1,26 +1,57 @@
-
 package csci.pkg446.project1;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
  * @author Mathew Gostnell
  */
 public class Generator {
+
+    private Random rng = new Random();
     private int numPnts;    // used for creating n points in Graph generation
     private ArrayList<Edge> edges;  // current list of edges of graph
     private ArrayList<Point> points;    // current list of points on graph
-    
+
     public Generator(int numPnts) {
         this.numPnts = numPnts;
+        points = new ArrayList<>();
+        edges = new ArrayList<>();
+        generatePoints();
+        generateEdges();
     }
     
+    public void generateNewGraph(int numPnts) {
+        this.numPnts = numPnts;
+        edges.clear();
+        points.clear();
+        generatePoints();
+        generateEdges();
+    }
+    
+    private void generateEdges() {
+        // while points are available
+        // pick a point X and find the closest point Y where Edge(X,Y) doesn't exist
+        // if Edge(X, Y) doesn't cross any other edger, create edge.  Repeat
+    }
+
+    private void generatePoints() {
+        while (points.size() < numPnts) {
+            double xCor = rng.nextDouble();
+            double yCor = rng.nextDouble();
+            if (isUnique(xCor, yCor) == true) {
+                points.add(new Point(xCor, yCor));
+            }
+        }
+    }
+
     /**
      * Checks to see if two edges intercept each other for Graph generation
-     * @param e1    Edge 1 used in intercept calculation
-     * @param e2    Edge 2 used in intercept calculation
-     * @return  True/False if e1 & e2 intercept each other
+     *
+     * @param e1 Edge 1 used in intercept calculation
+     * @param e2 Edge 2 used in intercept calculation
+     * @return True/False if e1 & e2 intercept each other
      */
     public boolean isIntercept(Edge e1, Edge e2) {
         // calculate the Point p where e1 and e2 would intercept if they were lines
@@ -28,33 +59,45 @@ public class Generator {
         double yCor = (xCor * e1.slope()) + e1.intercept();
         Point p = new Point(xCor, yCor);
         p.print();
-        
+
         // check to see if Point P is on line-segment e1 and e2
-        return onLine(p, e1) && onLine(p, e2);        
+        return onLine(p, e1) && onLine(p, e2);
     }
-    
+
     /**
      * Check to see if a point is on the same coordinates (x, y)
-     * @param p1    First point for comparison
-     * @param p2    Second point for comparison
-     * @return  boolean true if the p1 matches the (x, y) coordinates of p2
+     *
+     * @param p1 First point for comparison
+     * @param p2 Second point for comparison
+     * @return boolean true if the p1 matches the (x, y) coordinates of p2
      */
     private boolean match(Point p1, Point p2) {
         int check1 = Double.compare(p1.x(), p2.x());
         int check2 = Double.compare(p1.y(), p2.y());
         return check1 == 0 && check2 == 0;
     }
-    
+
+    private boolean isUnique(double x, double y) {
+        Point test = new Point(x, y);
+        boolean check = true;
+        for (int i = 0; i < points.size(); i++) {
+            if (match(points.get(i), test) == true) {
+                check = false;
+            }
+        }
+        return check;
+    }
+
     private boolean onLine(Point p, Edge e) {
         // check if p is within boundary box of e.start & e.end
         double minX = Math.min(e.start().x(), e.end().x());
         double maxX = Math.max(e.start().x(), e.end().x());
         double minY = Math.min(e.start().y(), e.end().y());
         double maxY = Math.max(e.start().y(), e.end().y());
-        
+
         System.out.println("Boundary: (" + minX + ", " + maxX + "), (" + minY + ", " + maxY + ")");
-        
+
         return minX <= p.x() && p.x() <= maxX && minY <= p.y() && p.y() <= maxY;
-        
+
     }
 }
