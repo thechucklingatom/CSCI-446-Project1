1deb57d1ffcff41ff6ffcb107e2b8805766fb32c
Robert Putnam <thechucklingatom@gmail.com>
2016-09-15 19:18:31 -0600

    Added abstract classes and graph class to hold graphs

---
+++ src/csci/pkg446/project1/Graph.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci.pkg446.project1;
+
+/**
+ *
+ * @author thech_000
+ */
+public class Graph {
+    
+}
