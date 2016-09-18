6b49000768cbf13b074f5df97afd8e658da910ac
Robert Putnam <thechucklingatom@gmail.com>
2016-09-18 14:13:35 -0600

    Added template for my graph solver algorithm, added problem solver class.

---
+++ src/csci/pkg446/project1/LocalSearch.java
@@ -0,0 +1,14 @@
+package csci.pkg446.project1;
+
+/**
+ *
+ * @author thechucklingatom
+ */
+public class LocalSearch extends Algorithm{
+
+    @Override
+    public Graph SolveGraph(Graph graphToSolve) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/csci/pkg446/project1/ProblemSolver.java
@@ -0,0 +1,16 @@
+
+package csci.pkg446.project1;
+
+/**
+ *
+ * @author thechucklingatom
+ */
+public class ProblemSolver {
+    public ProblemSolver(){
+        
+    }
+    
+    public Graph colorGraph(Graph graphToSolve, Algorithm solvingAlgorithm){
+        return solvingAlgorithm.SolveGraph(graphToSolve);
+    }
+}
