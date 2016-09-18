86390ebc0a8e22b677997076cf26685c7e1a8a68
Robert Putnam <thechucklingatom@gmail.com>
2016-09-18 11:03:08 -0600

    Graph functionality.

--- src/csci/pkg446/project1/Graph.java
+++ src/csci/pkg446/project1/Graph.java
@@ -1,11 +1,64 @@
 package csci.pkg446.project1;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author thechucklingatom
  */
-public class Graph implements Serializable{
+public final class Graph implements Serializable{
+    private List<Edge> edges;
+    private List<Point> points;
+    private int index;
     
+    public Graph(List<Edge> edges, List<Point> points){
+        updateGraph(edges, points);
+    }
+    
+    public void updateGraph(List<Edge> edges, List<Point> points){
+        this.edges = edges;
+        this.points = points;
+        index = 0;
+    }
+    
+    public List<Edge> getEdges(){
+        return edges;
+    }
+    
+    public List<Point> getPoints(){
+        return points;
+    }
+    
+    public Point getStartPoint(){
+        return points.get(0);
+    }
+    
+    public Point getCurrentPoint(){
+        return points.get(index);
+    }
+    
+    public Point getNextPoint(){
+        index++;
+        return points.get(index);
+    }
+    
+    public Point getPreviousPoint(){
+        if(index != 0){
+            index--;
+            return points.get(index);
+        }
+        return null;
+    }
+    
+    public List<Edge> getEdgesFromPoint(Point toGetEdgesFor){
+        List<Edge> toReturn = new ArrayList();
+        edges.stream().filter((edge) -> (edge.start().equals(toGetEdgesFor)
+                || edge.end().equals(toGetEdgesFor))).forEach((edge) -> {
+                    toReturn.add(edge);
+        });
+        
+        return toReturn;
+    }
 }
