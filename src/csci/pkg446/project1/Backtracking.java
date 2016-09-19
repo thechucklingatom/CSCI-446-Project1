a2b6fc17c3a0761be0c2cbc9d7df6cc7c12d8f62
fraticellics <fraticellics@gmail.com>
2016-09-18 22:56:53 -0600

    This should just add two method headers and some comments about the plan. Hopefully this doesn't screw everything up.

--- src/csci/pkg446/project1/Backtracking.java
+++ src/csci/pkg446/project1/Backtracking.java
@@ -2,8 +2,24 @@ package csci.pkg446.project1;
 
 /**
  *
- * @author thechucklingatom
+ * @author thechucklingatom, 765alpha
  */
 public abstract class Backtracking extends Algorithm {
     public abstract Graph solveGraph(Graph graphToSolve);
+    
+    public Graph simpleBacktracking(Graph graphToSolve){
+        Graph temp = graphToSolve;
+        return temp;
+        //this is just depth-first search. should be very easy and wil
+        //work on it monday/tuesday
+    }
+    
+    public Graph forwardBacktracking(Graph graphToSolve){
+        Graph temp = graphToSolve;
+        return temp;
+        //still need to read this section, but it can wait until simple
+        //has been finished.
+    }
+    
+    //This is exciting!!1!
 }
