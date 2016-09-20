1506cd53017c14926fea318a58f7e86ee8acd125
fraticellics <fraticellics@gmail.com>
2016-09-20 15:49:11 -0600

    Got rid of the methods I put. Thank you Robert for pointing that out. My stuff will be in their own class that extends this one.

--- src/csci/pkg446/project1/Backtracking.java
+++ src/csci/pkg446/project1/Backtracking.java
@@ -2,24 +2,8 @@ package csci.pkg446.project1;
 
 /**
  *
- * @author thechucklingatom, 765alpha
+ * @author thechucklingatom
  */
 public abstract class Backtracking extends Algorithm {
     public abstract Graph solveGraph(Graph graphToSolve);
-    
-    public Graph simpleBacktracking(Graph graphToSolve){
-        Graph temp = graphToSolve;
-        return temp;
-        //this is just depth-first search. should be very easy and wil
-        //work on it monday/tuesday
-    }
-    
-    public Graph forwardBacktracking(Graph graphToSolve){
-        Graph temp = graphToSolve;
-        return temp;
-        //still need to read this section, but it can wait until simple
-        //has been finished.
-    }
-    
-    //This is exciting!!1!
 }
