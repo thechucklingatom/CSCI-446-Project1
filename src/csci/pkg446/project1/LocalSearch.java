627d756353ed5c000aaea61f62072406a00f3e1c
Robert Putnam <thechucklingatom@gmail.com>
2016-09-22 10:14:14 -0600

    added equals and hash code to point.

--- src/csci/pkg446/project1/LocalSearch.java
+++ src/csci/pkg446/project1/LocalSearch.java
@@ -6,6 +6,18 @@ package csci.pkg446.project1;
  */
 public class LocalSearch extends Algorithm{
 
+    
+    /**
+     * pseudocode for hill climbing. will be using the amount of edges as the value
+     * function HILL-CLIMBING(problem) returns a state that is a local maximum
+     * current = MAKE-NODE(problem.lNITIAL-STATE)
+     * loop do
+     *  neighbor = a highest-valued successor of current
+     *  if neighbor.VALUE less than or equal current.VALUE then return current.STATE
+     * current = neighbor
+     * @param graphToSolve
+     * @return 
+     */
     @Override
     public Graph SolveGraph(Graph graphToSolve) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
