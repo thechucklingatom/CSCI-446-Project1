1059a3624a006a217b235b68ff52a9daaf28eb51
Mathew Gostnell <mathew.gostnell@gmail.com>
2016-09-23 12:13:34 -0600

    Almost working MinConflicts function

--- src/csci/pkg446/project1/MinConflicts.java
+++ src/csci/pkg446/project1/MinConflicts.java
@@ -5,6 +5,7 @@
  */
 package csci.pkg446.project1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -34,23 +35,20 @@ public class MinConflicts extends Algorithm {
         // Constraint := An edge with a Point of the same Color (+1)
         // will check graph with 50 starting comparisons (minConstraint 50 times)
         // this has been shown to solve most complex problems (like n-queens)
-
         Graph g = graphToSolve;
+        edges = g.getEdges();        
         points = randomColor(g);        
-        edges = g.getEdges();
-        for (Point p : points) {
-            updateEdges(p);
-        } // apply random color to edges
 
-        int maxCount = points.size() + 50;
+        // max number of mindecisions we will make
+        int maxCount = points.size() * points.size();
         int curCount = 0;
-        int index = curCount % points.size();
+        int index = 0;
         Point curPoint;
-        while (curCount < maxCount) {   // attempt 3 color solution
+        do {   // attempt 3 color solution
             // grab our current point from our list
             curPoint = points.get(index);
             // get all edges with our current point
-            List<Edge> curEdges = g.getEdgesFromPoint(curPoint);
+            List<Edge> curEdges = findEdges(curPoint);
             // find a color with the minimum number of conflicts at this Point
             String minConflict = getConflictColor(curEdges);
             // update our current Point color
@@ -61,23 +59,23 @@ public class MinConflicts extends Algorithm {
             updateEdges(curPoint);
             // return valid graph if it exists
             if (validGraph()) {
+                System.out.println("Valid Solution with 3 colors:\n\n");
                 return new Graph(edges, points);
             }
             // not valid graph, update index and continue finding miniumum conflicts
-            index = ++index % points.size();
+            index = rng.nextInt(points.size());
             curCount++;
-        }
+        } while (curCount < maxCount);
+        
+        System.out.println("Couldn't find 3-color solution.  Trying 4-color...");
         curColors = 4; // now try 4 color solution
         points = randomColor(g);
-        for (Point p : points) {
-            updateEdges(p);
-        }
         curCount = 0;
-        while (curCount < maxCount) {   // attempt 4 color solution
+        do {   // attempt 4 color solution
             // grab our current point from our list
             curPoint = points.get(index);
             // get all edges with our current point
-            List<Edge> curEdges = g.getEdgesFromPoint(curPoint);
+            List<Edge> curEdges = findEdges(curPoint);
             // find a color with the minimum number of conflicts at this Point
             String minConflict = getConflictColor(curEdges);
             // update our current Point color
@@ -88,91 +86,134 @@ public class MinConflicts extends Algorithm {
             updateEdges(curPoint);
             // return valid graph if it exists
             if (validGraph()) {
+                System.out.println("Valid Solution with 4 colors:\n\n");
                 return new Graph(edges, points);
             }
             // not valid graph, update index and continue finding miniumum conflicts
-            index = ++index % points.size();
+            index = rng.nextInt(points.size());
             curCount++;
-        }
-
+        } while (curCount < maxCount);
+        System.out.println("No solution found.  Try again!");
         return null;
     }
-    
+
     private boolean validGraph() {
         return edges.stream().noneMatch((e) -> (e.start().c().equals(e.end().c())));
     }
-    
+
     private void updateEdges(Point p) {
         edges.stream().forEach((e) -> {
-            if (e.start().equals(p)) {
+            if (e != null && e.start().equals(p)) {
                 e.start().setColor(p.c());
-            } else if (e.end().equals(p)) {
+            } else if (e != null && e.end().equals(p)) {
                 e.end().setColor(p.c());
             }
         });
     }
 
-    private String getConflictColor(List<Edge> edges) {
-        int index = 0;
-        int[] clrs;
-        if (curColors < 4) {
-            clrs = new int[3];
-            for (Edge curEdge : edges) {
-                String cnsColor = isConflict(curEdge);
-                switch (cnsColor) {
-                    case "Red":
-                        clrs[0]++;
-                        break;
-                    case "Green":
-                        clrs[1]++;
-                        break;
-                    case "Blue":
-                        clrs[2]++;
-                        break;
-                    default:
-                        break;
-                }
+    private List<Edge> findEdges(Point p) {
+        List<Edge> pairs = new ArrayList<>();
+        for (Edge e : this.edges) {
+            if (e.start().equals(p) || e.end().equals(p)) {
+                pairs.add(e);
             }
-            int conflicts = edges.size() + 1; // assumes all points same color
+        }
+        return pairs;
+    }
 
-            for (int i = 0; i < 3; i++) {
-                if (clrs[i] < conflicts) {
-                    conflicts = clrs[i];
-                    index = i;
-                }
-            }
-        } else {
-            clrs = new int[4];
-            for (Edge curEdge : edges) {
-                // count the color conflicts for these edges
-                String cnsColor = isConflict(curEdge);
-                switch (cnsColor) {
+    private String getConflictColor(List<Edge> edges) {
+        int[] colConflicts;
+        if (curColors < 4) {   // 3-color mode 
+            colConflicts = new int[3];
+            // count the conflicts and there are non-zero conflicts, find the min
+            // if there are zero conflicts, grab all of them and randomly select one
+            // counting conflicts
+            for (Edge e : edges) {
+                switch (isConflict(e)) {
                     case "Red":
-                        clrs[0]++;
-                        break;
-                    case "Green":
-                        clrs[1]++;
+                        colConflicts[0]++;
                         break;
                     case "Blue":
-                        clrs[2]++;
+                        colConflicts[1]++;
+                        break;
+                    case "Green":
+                        colConflicts[2]++;
+                        break;
+                    default:    // not a conflict!
+                        break;
+                }
+            }
+
+        } else { // 4-color mode
+            colConflicts = new int[4];
+
+            for (Edge e : edges) {
+                switch (isConflict(e)) {
+                    case "Red":
+                        colConflicts[0]++;
+                        break;
+                    case "Blue":
+                        colConflicts[1]++;
+                        break;
+                    case "Green":
+                        colConflicts[2]++;
                         break;
                     case "Yellow":
-                        clrs[3]++;
+                        colConflicts[3]++;
                         break;
-                    default:
+                    default:    // not a conflict!
                         break;
-                }
-            }
-            int conflicts = edges.size() + 1; // assumes all points same color
-
-            for (int i = 0; i < 4; i++) {
-                if (clrs[i] < conflicts) {
-                    conflicts = clrs[i];
-                    index = i;
                 }
             }
         }
-        switch (index) {
+
+        // check for the min of all non-zeros or the presence of non-zeros
+        List<Integer> matchIndex = new ArrayList<>();
+        List<Integer> conflictIndex = new ArrayList<>();
+        int curMin = Integer.MAX_VALUE;
+        int colIndex = -1;
+
+        for (int i = 0; i < colConflicts.length; i++) {
+            if (colConflicts[i] == 0) {
+                conflictIndex.add(i);
+            } else if (colConflicts[i] < curMin) {
+                matchIndex.clear();
+                curMin = colConflicts[i];
+                colIndex = i;
+            } else if (colConflicts[i] == curMin) {
+                matchIndex.add(i);
+                colIndex = -1;
+            }
+        }
+        if (conflictIndex.size() > 0) {
+            // return random color from zero-conflicts list
+            return getRandomColor(conflictIndex);
+        } else if (matchIndex.size() > 0) {
+            // return random color from matching min-conflict colors
+            return getRandomColor(matchIndex);
+        } else if (colIndex != -1 && colIndex != Integer.MAX_VALUE) {
+            // return color that matches the minimum colors
+            switch (colIndex) {
+                case 0:
+                    return "Red";
+                case 1:
+                    return "Blue";
+                case 2:
+                    return "Green";
+                case 3:
+                    return "Yellow";
+                default:
+                    return " ";
+            }
+        } else {
+            return " ";
+        }
+    }
+
+    private String getRandomColor(List<Integer> possibleColors) {
+        int index = rng.nextInt(possibleColors.size());
+        int value = possibleColors.get(index);
+        switch (value) {
             case 0:
                 return "Red";
             case 1:
@@ -182,62 +223,38 @@ public class MinConflicts extends Algorithm {
             case 3:
                 return "Yellow";
             default:
-                return "Colorless";
+                return getRandomColor(possibleColors);
         }
-    }
-
-    private List<Point> randomColor(Graph g) {
-        List<Point> rPoints = g.getPoints();
-        for (Point p : rPoints) {
-            int rnd = rng.nextInt();
-            if (curColors < 4) {
-                switch (rnd % 3) {
-                    case 0:
-                        p.setColor("Red");
-                        break;
-                    case 1:
-                        p.setColor("Blue");
-                        break;
-                    default:
-                        p.setColor("Green");
-                        break;
-                } // <4 colors
-            } else {
-                switch (rnd % 4) {
-                    case 0:
-                        p.setColor("Red");
-                        break;
-                    case 1:
-                        p.setColor("Blue");
-                        break;
-                    case 2:
-                        p.setColor("Green");
-                        break;
-                    default:
-                        p.setColor("Yellow");
-                        break;
-                }
-            } // >= 4 colors
-        }
-        return rPoints;
-    }
-
-    private List<Edge> findEdges(Point p, List<Edge> edges) {
-        List<Edge> match = edges;
-        match.clear();
-        for (Edge e : edges) {
-            if (e.start().equals(p) || e.end().equals(p)) {
-                match.add(e);
-            }
-        }
-        return match;
     }
 
     private String isConflict(Edge e) {
         if (e.start().c().equals(e.end().c())) {
             return e.start().c();
         } else {
-            return "Colorless";
+            return ""; //default case is empty string 
         }
+    }
+
+    private List<Point> randomColor(Graph g) {
+        List<Point> pnts = g.getPoints();
+        for (Point p : pnts) {
+            int rnd = rng.nextInt(curColors);
+            switch (rnd) {
+                case 0:
+                    p.setColor("Red");
+                    break;
+                case 1:
+                    p.setColor("Blue");
+                    break;
+                case 2:
+                    p.setColor("Green");
+                    break;
+                case 3:
+                    p.setColor("Yellow");
+                    break;
+            }
+            updateEdges(p);
+        }
+        return pnts;
     }
 }
